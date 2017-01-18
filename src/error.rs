bb358ab6825956943e053343c8f4f4a49750c063
Stephen Oliver <steve@infincia.com>
2017-01-18 18:49:06 -0500

    implement std::error::Error and std:fmt::Display on Bip39Error

--- src/error.rs
+++ src/error.rs
@@ -7,12 +7,62 @@ pub enum Bip39Error {
     InvalidWord,
     InvalidKeysize,
     InvalidWordLength,
-    EntropyUnavailable,
+    EntropyUnavailable(std::io::Error),
     LanguageUnavailable
 }
 
 impl From<std::io::Error> for Bip39Error {
-    fn from(_: std::io::Error) -> Bip39Error {
-        return Bip39Error::EntropyUnavailable
+    fn from(err: std::io::Error) -> Bip39Error {
+        return Bip39Error::EntropyUnavailable(err)
+    }
+}
+
+impl std::fmt::Display for Bip39Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        match *self {
+            Bip39Error::InvalidChecksum => {
+                write!(f, "Invalid checksum")
+            },
+            Bip39Error::InvalidWord => {
+                write!(f, "Invalid word in phrase")
+            },
+            Bip39Error::InvalidKeysize => {
+                write!(f, "Invalid keysize")
+            },
+            Bip39Error::InvalidWordLength => {
+                write!(f, "Invalid number of words in phrase")
+            },
+            Bip39Error::EntropyUnavailable(ref err) => {
+                write!(f, "Entropy unavailable: {}", err)
+            },
+            Bip39Error::LanguageUnavailable => {
+                write!(f, "Language unavailable")
+            },
+        }
+    }
+}
+
+
+impl std::error::Error for Bip39Error {
+    fn description(&self) -> &str {
+        match *self {
+            Bip39Error::InvalidChecksum => "invalid checksum",
+            Bip39Error::InvalidWord => "invalid word in phrase",
+            Bip39Error::InvalidKeysize => "invalid keysize",
+            Bip39Error::InvalidWordLength => "invalid number of words in phrase",
+            Bip39Error::EntropyUnavailable(ref err) => err.description(),
+            Bip39Error::LanguageUnavailable => "wrapping key failed",
+        }
+    }
+
+    fn cause(&self) -> Option<&std::error::Error> {
+        match *self {
+            Bip39Error::InvalidChecksum => None,
+            Bip39Error::InvalidWord => None,
+            Bip39Error::InvalidKeysize => None,
+            Bip39Error::InvalidWordLength => None,
+            Bip39Error::EntropyUnavailable(ref err) => Some(err),
+            Bip39Error::LanguageUnavailable => None,
+        }
     }
 }
