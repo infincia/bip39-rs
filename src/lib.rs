4e3b09730e0ea1916a507e0420b807e0a37b96a9
Stephen Oliver <steve@infincia.com>
2017-08-03 17:29:10 -0400

    Add quick start example to docs in crate root

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,38 @@
 //!
 //! [bip39-standard]: https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki
 //!
+//! ## Quickstart
+//!
+//! ```rust
+//! use bip39::{Mnemonic, MnemonicType, Language, Seed};
+//!
+//! /// determines the number of words in the mnemonic phrase
+//! let mnemonic_type = MnemonicType::Type12Words;
+//!
+//! /// create a new randomly generated mnemonic phrase
+//! let mnemonic = match Mnemonic::new(mnemonic_type, Language::English, "") {
+//!     Ok(b) => b,
+//!     Err(e) => { println!("e: {}", e); return }
+//! };
+//!
+//! /// get the phrase as a string
+//! let phrase = mnemonic.get_string();
+//! println!("phrase: {}", phrase);
+//!
+//! /// get the HD wallet seed
+//! let seed = mnemonic.get_seed();
+//!
+//! // get the HD wallet seed as raw bytes
+//! let seed_bytes: &[u8] = seed.as_ref();
+//!
+//! // get the HD wallet seed as a hex string
+//! let seed_hex: &str = seed.as_hex();
+//!
+//! // get an owned Seed instance
+//! let owned_seed: Seed = seed.to_owned();
+//!
+//! ```
+//!
 #[macro_use] extern crate error_chain;
 #[macro_use] extern crate lazy_static;
 extern crate data_encoding;
