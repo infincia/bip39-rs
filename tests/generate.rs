f05425b59bb155d82d8777246e57b2d98ac24d29
Stephen Oliver <steve@infincia.com>
2017-08-03 17:27:25 -0400

    Update mnemonic generation tests to use MnemonicType

--- tests/generate.rs
+++ tests/generate.rs
@@ -1,96 +1,96 @@
 extern crate bip39;
 
-use ::bip39::{Mnemonic, KeyType, Language};
+use ::bip39::{Mnemonic, MnemonicType, Language};
 
 
 #[test]
 fn generate_12_english() {
-    let kt = KeyType::for_word_length(12).unwrap();
+    let mnemonic_type = MnemonicType::for_word_count(12).unwrap();
 
-    let b = match Mnemonic::new(kt, Language::English, "") {
+    let mnemonic= match Mnemonic::new(mnemonic_type, Language::English, "") {
         Ok(b) => b,
         Err(_) => { assert!(false); return }
     };
-    let phrase = b.get_string();
+    let phrase = mnemonic.get_string();
     let words: Vec<&str> = phrase.split(" ").into_iter().collect();
 
     assert!(words.len() == 12);
-    assert!(words.len() == kt.word_length());
-    let seed = b.get_seed();
+    assert!(words.len() == mnemonic_type.word_count());
+    let seed = mnemonic.get_seed();
     let seed_bytes: &[u8] = seed.as_bytes();
     assert!(seed_bytes.len() == 64);
 }
 
 #[test]
 fn generate_15_english() {
-    let kt = KeyType::for_word_length(15).unwrap();
+    let mnemonic_type = MnemonicType::for_word_count(15).unwrap();
 
-    let b = match Mnemonic::new(kt, Language::English, "") {
+    let mnemonic= match Mnemonic::new(mnemonic_type, Language::English, "") {
         Ok(b) => b,
         Err(_) => { assert!(false); return }
     };
-    let phrase = b.get_string();
+    let phrase = mnemonic.get_string();
     let words: Vec<&str> = phrase.split(" ").into_iter().collect();
 
     assert!(words.len() == 15);
-    assert!(words.len() == kt.word_length());
-    let seed = b.get_seed();
+    assert!(words.len() == mnemonic_type.word_count());
+    let seed = mnemonic.get_seed();
     let seed_bytes: &[u8] = seed.as_bytes();
     assert!(seed_bytes.len() == 64);
 }
 
 #[test]
 fn generate_18_english() {
-    let kt = KeyType::for_word_length(18).unwrap();
+    let mnemonic_type = MnemonicType::for_word_count(18).unwrap();
 
-    let b = match Mnemonic::new(kt, Language::English, "") {
+    let mnemonic= match Mnemonic::new(mnemonic_type, Language::English, "") {
         Ok(b) => b,
         Err(_) => { assert!(false); return }
     };
-    let phrase = b.get_string();
+    let phrase = mnemonic.get_string();
     let words: Vec<&str> = phrase.split(" ").into_iter().collect();
 
     assert!(words.len() == 18);
-    assert!(words.len() == kt.word_length());
-    let seed = b.get_seed();
+    assert!(words.len() == mnemonic_type.word_count());
+    let seed = mnemonic.get_seed();
     let seed_bytes: &[u8] = seed.as_bytes();
     assert!(seed_bytes.len() == 64);
 }
 
 #[test]
 fn generate_21_english() {
-    let kt = KeyType::for_word_length(21).unwrap();
+    let mnemonic_type = MnemonicType::for_word_count(21).unwrap();
 
-    let b = match Mnemonic::new(kt, Language::English, "") {
+    let mnemonic= match Mnemonic::new(mnemonic_type, Language::English, "") {
         Ok(b) => b,
         Err(_) => { assert!(false); return }
     };
 
-    let phrase = b.get_string();
+    let phrase = mnemonic.get_string();
     let words: Vec<&str> = phrase.split(" ").into_iter().collect();
 
     assert!(words.len() == 21);
-    assert!(words.len() == kt.word_length());
-    let seed = b.get_seed();
+    assert!(words.len() == mnemonic_type.word_count());
+    let seed = mnemonic.get_seed();
     let seed_bytes: &[u8] = seed.as_bytes();
     assert!(seed_bytes.len() == 64);
 }
 
 #[test]
 fn generate_24_english() {
-    let kt = KeyType::for_word_length(24).unwrap();
+    let mnemonic_type = MnemonicType::for_word_count(24).unwrap();
 
-    let b = match Mnemonic::new(kt, Language::English, "") {
+    let mnemonic= match Mnemonic::new(mnemonic_type, Language::English, "") {
         Ok(b) => b,
         Err(_) => { assert!(false); return }
     };
 
-    let phrase = b.get_string();
+    let phrase = mnemonic.get_string();
     let words: Vec<&str> = phrase.split(" ").into_iter().collect();
 
     assert!(words.len() == 24);
-    assert!(words.len() == kt.word_length());
-    let seed = b.get_seed();
+    assert!(words.len() == mnemonic_type.word_count());
+    let seed = mnemonic.get_seed();
     let seed_bytes: &[u8] = seed.as_bytes();
     assert!(seed_bytes.len() == 64);
 }
