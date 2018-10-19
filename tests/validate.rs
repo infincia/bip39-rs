2cc26b83baa883ba96203ab43dd224f947708aa3
maciejhirsz <maciej.hirsz@gmail.com>
2018-10-19 14:43:28 +0200

    Test all languages

--- tests/validate.rs
+++ tests/validate.rs
@@ -1,6 +1,25 @@
 extern crate bip39;
 
-use ::bip39::{Mnemonic, Language};
+use ::bip39::{Mnemonic, MnemonicType, Language};
+
+fn validate_language(lang: Language) {
+    let types = &[
+        MnemonicType::Words12,
+        MnemonicType::Words15,
+        MnemonicType::Words18,
+        MnemonicType::Words21,
+        MnemonicType::Words24,
+    ];
+
+    for mtype in types {
+        for _ in 0..1000 {
+            let m1 = Mnemonic::new(*mtype, lang);
+            let m2 = Mnemonic::from_phrase(m1.phrase(), lang).expect("Can create a Mnemonic");
+
+            assert_eq!(m1.entropy(), m2.entropy());
+        }
+    }
+}
 
 #[test]
 fn validate_12_english() {
@@ -45,4 +64,44 @@ fn validate_12_english_uppercase() {
     let invalid_phrase = "Park remain person kitchen mule spell knee armed position rail grid ankle";
 
     assert!(Mnemonic::from_phrase(invalid_phrase, Language::English).is_err());
+}
+
+#[test]
+fn validate_english() {
+    validate_language(Language::English);
+}
+
+#[test]
+fn validate_chinese_simplified() {
+    validate_language(Language::ChineseSimplified);
+}
+
+#[test]
+fn validate_chinese_traditional() {
+    validate_language(Language::ChineseTraditional);
+}
+
+#[test]
+fn validate_french() {
+    validate_language(Language::French);
+}
+
+#[test]
+fn validate_italian() {
+    validate_language(Language::Italian);
+}
+
+#[test]
+fn validate_japanese() {
+    validate_language(Language::Japanese);
+}
+
+#[test]
+fn validate_korean() {
+    validate_language(Language::Korean);
+}
+
+#[test]
+fn validate_spanish() {
+    validate_language(Language::Spanish);
 }
